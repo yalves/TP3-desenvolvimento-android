85c038793c2cdb3ee9e645a56a1f712ac0fb6b98
Yan Alves Monteiro <Yan Alves Monteiro>
2017-09-28 01:31:30 -0300

    :construction: Adiciona firebase ao projeto

---
+++ Tp3.YanAlves.Android/app/src/main/java/com/example/yanal/tp3yanalvesandroid/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.yanal.tp3yanalvesandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference databaseNode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        databaseNode = database.getReference("teste");
+
+        databaseNode.setValue("Yan");
+    }
+}
