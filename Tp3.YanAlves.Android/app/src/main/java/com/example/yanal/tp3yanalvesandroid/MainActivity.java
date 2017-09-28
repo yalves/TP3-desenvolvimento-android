748aa07524023363a701e4782cad6ad4fe5131e8
Yan Alves Monteiro <yanal@DESKTOP-A6FG6AP.(none)>
2017-09-28 17:27:25 -0300

    :construction: Adiciona funcionalidade ao botao de limpar

--- Tp3.YanAlves.Android/app/src/main/java/com/example/yanal/tp3yanalvesandroid/MainActivity.java
+++ Tp3.YanAlves.Android/app/src/main/java/com/example/yanal/tp3yanalvesandroid/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.yanal.tp3yanalvesandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         databaseNode = database.getReference("Usuarios");
+    }
+
+    public void LimparFormulario(View view)
+    {
+        List<EditText> campos = new ArrayList<>();
+
+        campos.add((EditText)findViewById(R.id.txtBoxCelular));
+        campos.add((EditText)findViewById(R.id.txtBoxCidade));
+        campos.add((EditText)findViewById(R.id.txtBoxCpf));
+        campos.add((EditText)findViewById(R.id.txtBoxEmail));
+        campos.add((EditText)findViewById(R.id.txtBoxNome));
+        campos.add((EditText)findViewById(R.id.txtBoxSenha));
+        campos.add((EditText)findViewById(R.id.txtBoxTelefone));
+
+        LimparCampos(campos);
+    }
+
+    private void LimparCampos(List<EditText> campos)
+    {
+        for (EditText campo: campos) {
+            campo.setText("");
+        }
     }
 }
